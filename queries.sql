5752e69c686192c137fc3050a9e0d0b8de200c34
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-05-09 16:35:38 +0200

    Revert "Add basic sql functionality"

--- queries.sql
+++ queries.sql
@@ -1,11 +1,3 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-select * from animals;
-select * from animals where name like '%mon';
-select name from animals where date_of_birth between '2016-01-01' AND '2019-12-31';
-select name from animals where neutered=true and escape_attempts<3;
-select date_of_birth from animals where name='Agumon' or name='Pikachu';
-select name, escape_attempts from animals where weight_kg > 10.5;
-select * from animals where neutered=true;
-SELECT * from animals WHERE name <> 'Gabumon';
-select * from animals where weight_kg between 10.4 and 17.3;
+SELECT * from animals WHERE name = 'Luna';
--- schema.sql
+++ schema.sql
@@ -1,19 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
-/* Create the database */
-CREATE DATABASE vet_clinic;
-
-/* Switch to the vet_clinic database */
-USE vet_clinic;
-
-/* Create the animals table */
-
-create table animals(
-	id INTEGER PRIMARY KEY AUTOINCREMENT ,
-	name varchar(255),
-	date_of_birth DATE,
-	escape_attempts INTEGER,
-	neutered BOOLEAN,
-	weight_kg DECIMAL(10,2)
-	
+CREATE TABLE animals (
+    name varchar(100)
 );
