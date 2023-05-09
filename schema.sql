81b6263e85d310ab8bc408dda272e9cb9f3c70f7
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-05-09 16:48:53 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,19 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+/* Create the database */
+CREATE DATABASE vet_clinic;
+
+/* Switch to the vet_clinic database */
+USE vet_clinic;
+
+/* Create the animals table */
+
+create table animals(
+	id INTEGER PRIMARY KEY AUTOINCREMENT ,
+	name varchar(255),
+	date_of_birth DATE,
+	escape_attempts INTEGER,
+	neutered BOOLEAN,
+	weight_kg DECIMAL(10,2)
+	
 );
