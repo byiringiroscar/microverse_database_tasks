9a360941a9b305aadac5843ab76dd990cb19cc22
byiringiroscar <byiringoroscar@gmail.com>
2023-05-11 20:04:23 +0200

    Add required querries

--- schema.sql
+++ schema.sql
@@ -50,3 +50,32 @@ ADD COLUMN owner_id  INT,
 ADD CONSTRAINT fk_owner_id 
   FOREIGN KEY (owner_id )
   REFERENCES owners (id);
+
+
+CREATE TABLE vets(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(255),
+	age INT,
+	date_of_graduation DATE,
+	PRIMARY KEY(id)
+
+);
+
+CREATE TABLE specializations (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  vet_id INT,
+  species_id INT,
+  PRIMARY KEY (id, vet_id, species_id),
+  FOREIGN KEY (vet_id) REFERENCES vets(id),
+  FOREIGN KEY (species_id) REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animal_id INT,
+  vet_id INT,
+  visit_date DATE,
+  PRIMARY KEY (id, animal_id, vet_id),
+  FOREIGN KEY (animal_id) REFERENCES animals(id),
+  FOREIGN KEY (vet_id) REFERENCES vets(id)
+);
